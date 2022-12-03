3f8201ef360065e6d992553f1f79f67722f1ebb2
NickSidin <nick_sidin@bk.ru>
2022-12-03 17:28:00 +0300

    commit

--- PZ_10/PZ_10.py
+++ PZ_10/PZ_10.py
@@ -2,12 +2,14 @@
 #Известны марки машин, выпускаемые в данной стране и экспортируемых в N заданных стран.
 #Определить какие марки машин были доставлены во все указанные страны,
 #какие в некоторые из стран и какие не доставлены ни в одну страну.
-
-cars = {'BMW', 'Mercedes', 'Audi', 'Nissan'}
-russia = {'BMW', 'Nissan'}
-france = {'Nissan', 'Mercedes'}
-lithuania = {'Nissan', 'BMW'}
-italy = {'Mercedes', 'Nissan', 'BMW'}
-print('Во все страны были доставлены: ', russia & france & lithuania & italy)
-print('В некоторые страны были доставлены: ', russia | france | lithuania | italy)
-print('Ни в одну страну не были доставлены: ', cars - russia - france - lithuania - italy)
+try:
+    cars = {'BMW', 'Mercedes', 'Audi', 'Nissan'}
+    russia = {'BMW', 'Nissan'}
+    france = {'Nissan', 'Mercedes'}
+    lithuania = {'Nissan', 'BMW'}
+    italy = {'Mercedes', 'Nissan', 'BMW'}
+    print('Во все страны были доставлены: ', russia & france & lithuania & italy)
+    print('В некоторые страны были доставлены: ', russia | france | lithuania | italy)
+    print('Ни в одну страну не были доставлены: ', cars - russia - france - lithuania - italy)
+except:
+    print('Error')
